c5559f2746d36db09b1b6c463acb1441da57a675
Varad Nirgude <108128032+varadnir@users.noreply.github.com>
2025-03-10 12:51:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-__import__('pysqlite3')
-import sys
-sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
+#__import__('pysqlite3')
+#import sys
+#sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
+import sqlite3
 import os
 import chromadb
 import streamlit as st
